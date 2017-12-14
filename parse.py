c7dd3aa9ab2b56b6a85b5e5267931b4867dc8a38
Gabriel Dugny <gabriel.dugny@gmail.com>
2017-12-14 19:39:59 +0100

    WIP matching parenthesis

--- parse.py
+++ parse.py
@@ -12,7 +12,14 @@ def _get_tuple(token: str) -> tuple:
     elif token in "*/":
         return token, "operator", 1
     elif token in "()":
-        return token, "parenthesis"
+        global parenthesis_counter
+        if token == "(":
+            parenthesis_counter += 1
+            parenthesis_id = parenthesis_counter
+        elif token == ")":
+            parenthesis_counter -= 1
+            parenthesis_id = parenthesis_counter - 1
+        return token, "parenthesis", parenthesis_id
     else:
         try:
             return int(token), "integer"
@@ -22,13 +29,17 @@ def _get_tuple(token: str) -> tuple:
 
 def parse(expression: str) -> list:
     list_expression = _expression_to_list(expression)
+    global parenthesis_counter
+    parenthesis_counter = 0
     for i in range(len(list_expression)):
-        list_expression[i] = _get_tuple(list_expression[i])
+        list_expression[i] = _get_tuple(list_expression[i], parenthesis_counter)
+    print(list_expression)
     return list_expression
 
 
 if __name__ == "__main__":
-    print(parse("3+4*2"))
-    print(parse("(3 + 2) * 4"))
+    # print(parse("3+4*2"))
+    # print(parse("(3 + 2) * 4"))
     print(parse(" ( 3+2 )* 4 "))
-    print(parse("(3+254)* 44 "))
+    print(parse("((3+(2))*4)"))
+    # print(parse("(3+254)* 44 "))
