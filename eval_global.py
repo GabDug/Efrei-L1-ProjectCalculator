b33b9e5dba886688e9b35649a75815f903528837
Thibault Lepez <tibolepezfr@gmail.com>
2017-12-22 12:47:21 +0100

    Started the eval_global() function and added single_element recognition

--- eval_global.py
+++ eval_global.py
@@ -1,5 +1,27 @@
-from parse import parse
+from eval_int import *
+
+
+def single_element(element):
+    if element[1] == "variable":
+        if element[0] == "exit":
+            exit()
+        else:
+            return element[3]
+    elif element[1] == "integer":
+        return element[0]
 
 
 def eval_global(expression):
-    pass
+    size = len(expression)
+
+    if size == 0:
+        return None
+    elif size == 1:
+        return single_element(expression[0])
+
+    else:
+        return eval_int(expression)
+
+
+if __name__ == "__main__":
+    print(eval_global(parse("1")))
